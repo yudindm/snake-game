879df6363e5a05b4ed1b6dac6312ebb0fc64f667
Dmitry Yudin <yudindm@mail.ru>
2015-12-19 11:54:39 +0300

    Метод grow для змеи с координатами

--- lib/snake.ex
+++ lib/snake.ex
@@ -5,14 +5,43 @@ defmodule Snake do
     %Snake{h: {x, y}, tail: [{x, y}]}
   end
 
-  def new([{x, y} | tail]) do
-    %Snake{h: {x, y}, tail: parse_tail({x, y}, tail)}
+  def new([h | tail]) do
+    %Snake{h: h, tail: parse_tail(h, tail)}
   end
 
-  defp parse_tail({x, y}, []), do: []
-  defp parse_tail({x1, y1}, [{x2, y2} | tail]) do
-    raise_if_skew {x1, y1}, {x2, y2}
-    parse_tail({x2, y2}, tail) ++ [{x2, y2}]
+  def grow(snake, grow_dir, len \\ 1) do 
+    cur_dir = dir(snake)
+    if cur_dir == :none or cur_dir == grow_dir do
+      %Snake{snake | h: move_point(snake.h, grow_dir, len)}
+    else
+      %Snake{h: move_point(snake.h, grow_dir, len), tail: snake.tail ++ [snake.h]}
+    end
+  end
+
+  defp move_point({x, y}, :right, len), do: {x + len, y}
+  defp move_point({x, y}, :left, len),  do: {x - len, y}
+  defp move_point({x, y}, :down, len),  do: {x, y + len}
+  defp move_point({x, y}, :up, len),    do: {x, y - len}
+  defp move_point({x, y}, :none, _),    do: {x, y}
+
+  defp parse_tail(_, []), do: []
+  defp parse_tail(h, [p | tail]) do
+    raise_if_skew h, p
+    parse_tail(p, tail) ++ [p]
+  end
+
+  defp dir(%Snake{h: h, tail: tail}) do
+    p = Enum.fetch!(tail, -1)
+    dir(p, h)
+  end
+  defp dir({xs, ys}, {xe, ye}) do
+    cond do
+      xe > xs -> :right
+      xe < xs -> :left
+      ye > ys -> :down
+      ye < ys -> :up
+      true -> :none
+    end
   end
 
   defp raise_if_skew({x1, y1}, {x2, y2}) do
--- test/snake_test.exs
+++ test/snake_test.exs
@@ -3,12 +3,12 @@ defmodule SnakeTest do
 
   test "create snake at any position" do
     s = Snake.new(10, 20) 
-    assert ^s = %Snake{h: {10, 20}, tail: [{10, 20}]}
+    assert %Snake{h: {10, 20}, tail: [{10, 20}]} = s
   end
 
   test "create snake from point array" do
     s = Snake.new([{10, 20}, {10, 25}, {15, 25}]) 
-    assert ^s = %Snake{h: {10, 20}, tail: [{15, 25}, {10, 25}]}
+    assert %Snake{h: {10, 20}, tail: [{15, 25}, {10, 25}]} = s
   end
 
   test "snake must be orthogonal" do
@@ -17,8 +17,24 @@ defmodule SnakeTest do
     end
   end
 
-  test "grow snake by one up" do
-    s = Snake.new(10, 20) |> Snake.grow(:up)
-    assert ^s = %Snake{h: {10, 19}, tail: [{10, 20}]}
+  test "grow snake" do
+    s = Snake.new(10, 20)
+
+    assert %Snake{h: {10, 19}, tail: [{10, 20}]} = Snake.grow(s, :up)
+    assert %Snake{h: {10, 17}, tail: [{10, 20}]} = Snake.grow(s, :up, 3)
+    assert %Snake{h: {10, 21}, tail: [{10, 20}]} = Snake.grow(s, :down)
+    assert %Snake{h: {10, 23}, tail: [{10, 20}]} = Snake.grow(s, :down, 3)
+    assert %Snake{h: { 9, 20}, tail: [{10, 20}]} = Snake.grow(s, :left)
+    assert %Snake{h: { 7, 20}, tail: [{10, 20}]} = Snake.grow(s, :left, 3)
+    assert %Snake{h: {11, 20}, tail: [{10, 20}]} = Snake.grow(s, :right)
+    assert %Snake{h: {13, 20}, tail: [{10, 20}]} = Snake.grow(s, :right, 3)
+
+    s = Snake.new([{10, 20}, {30, 20}])
+    assert %Snake{h: {9, 20}, tail: [{30, 20}]} = Snake.grow(s, :left)
+    assert %Snake{h: {10, 19}, tail: [{30, 20}, {10, 20}]} = Snake.grow(s, :up)
+
+    s = Snake.new([{10, 20}, {30, 20}, {30, 40}])
+    assert %Snake{h: {9, 20}, tail: [{30, 40}, {30, 20}]} = Snake.grow(s, :left)
+    assert %Snake{h: {10, 19}, tail: [{30, 40}, {30, 20}, {10, 20}]} = Snake.grow(s, :up)
   end
 end
